724171017b9af1a4042479efd08eaf1726d6c7df
Emmanuel <emmanuelfolorunsho@icloud.com>
2022-01-12 13:07:26 +0000

    traverse linked list

---
+++ linked-list/traverse.js
@@ -0,0 +1,20 @@
+const linkedList = {
+  val: 5, 
+  next: {
+    val: 10,
+    next: {
+      val: 3, 
+      next: null,
+    }
+  }
+}
+
+const arr = [];
+let head = linkedList;
+
+while(head != null) {
+  arr.push(head.val)
+  head = head.next
+}
+
+console.log(arr)
