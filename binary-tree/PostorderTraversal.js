f974b5d952f8eb184fb139173871990ea8b80c2b
Emmanuel <emmanuelfolorunsho@icloud.com>
2022-01-18 13:53:34 +0000

    binary tree traversal postorder

--- binary-tree/PostorderTraversal.js
+++ binary-tree/PostorderTraversal.js
@@ -11,18 +11,18 @@ class BinaryTreeNode
 }
 
 let root = null
-root = new BinaryTreeNode(1)
-root.left = new BinaryTreeNode(2)
-root.left.left = new BinaryTreeNode(3)
-root.left.left.right = new BinaryTreeNode(4)
+root = new BinaryTreeNode(11)
+root.left = new BinaryTreeNode(7)
+root.left.left = new BinaryTreeNode(2)
+root.left.left.right = new BinaryTreeNode(1)
 
-root.left.right = new BinaryTreeNode(5)
-root.left.right.right = new BinaryTreeNode(7)
-root.left.right.left = new BinaryTreeNode(6)
+root.left.right = new BinaryTreeNode(6)
+root.left.right.right = new BinaryTreeNode(5)
+root.left.right.left = new BinaryTreeNode(4)
 
-root.right = new BinaryTreeNode(8)
-root.right.right = new BinaryTreeNode(10)
-root.right.left = new BinaryTreeNode(9)
+root.right = new BinaryTreeNode(10)
+root.right.right = new BinaryTreeNode(9)
+root.right.left = new BinaryTreeNode(8)
 
 function postOrder(node) {
    let output = []
